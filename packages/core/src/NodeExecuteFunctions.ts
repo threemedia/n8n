4ef96e812d7dc165db84fda3424aff43fc722186
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-04-05 15:03:17 +0200

    Update NodeExecuteFunctions.ts and add tests for proxyRequestToAxios

--- packages/core/src/NodeExecuteFunctions.ts
+++ packages/core/src/NodeExecuteFunctions.ts
@@ -85,18 +85,14 @@ import { IncomingMessage } from 'http';
 import { stringify } from 'qs';
 import type { Token } from 'oauth-1.0a';
 import clientOAuth1 from 'oauth-1.0a';
-import type {
-	ClientOAuth2Options,
-	ClientOAuth2RequestObject,
-	ClientOAuth2TokenData,
-} from '@n8n/client-oauth2';
+import type { ClientOAuth2Options, ClientOAuth2TokenData } from '@n8n/client-oauth2';
 import { ClientOAuth2 } from '@n8n/client-oauth2';
 import crypto, { createHmac } from 'crypto';
 import get from 'lodash/get';
 import type { Request, Response } from 'express';
 import FormData from 'form-data';
 import path from 'path';
-import type { OptionsWithUri, OptionsWithUrl, RequestCallback, RequiredUriUrl } from 'request';
+import type { OptionsWithUri, OptionsWithUrl, RequiredUriUrl } from 'request';
 import type { RequestPromiseOptions } from 'request-promise-native';
 import requestPromise from 'request-promise-native';
 import FileType from 'file-type';
@@ -598,14 +594,12 @@ export async function proxyRequestToAxios(
 	additionalData: IWorkflowExecuteAdditionalData,
 	node: INode,
 	uriOrObject: string | IDataObject,
-	options?: IDataObject,
 ): Promise<any> {
 	// Check if there's a better way of getting this config here
 	if (process.env.N8N_USE_DEPRECATED_REQUEST_LIB) {
 		return requestPromiseWithDefaults.call(
 			null,
 			uriOrObject as unknown as RequiredUriUrl & RequestPromiseOptions,
-			options as unknown as RequestCallback,
 		);
 	}
 
@@ -620,7 +614,7 @@ export async function proxyRequestToAxios(
 	if (uriOrObject !== undefined && typeof uriOrObject === 'object') {
 		configObject = uriOrObject;
 	} else {
-		configObject = options || {};
+		configObject = {};
 	}
 
 	axiosConfig = Object.assign(axiosConfig, await parseRequestObject(configObject));
@@ -671,7 +665,7 @@ export async function proxyRequestToAxios(
 				headers: response.headers,
 				statusCode: response.status,
 				statusMessage: response.statusText,
-				request: response.request,
+				// request: response.request,
 			};
 		} else {
 			let body = response.data;
@@ -1066,14 +1060,11 @@ async function prepareBinaryData(
 
 /**
  * Makes a request using OAuth data for authentication
- *
- * @param {(OptionsWithUri | requestPromise.RequestPromiseOptions)} requestOptions
- *
  */
 export async function requestOAuth2(
 	this: IAllExecuteFunctions,
 	credentialsType: string,
-	requestOptions: OptionsWithUri | requestPromise.RequestPromiseOptions | IHttpRequestOptions,
+	requestOptions: clientOAuth2.RequestObject,
 	node: INode,
 	additionalData: IWorkflowExecuteAdditionalData,
 	oAuth2Options?: IOAuth2Options,
@@ -1137,7 +1128,7 @@ export async function requestOAuth2(
 	);
 	// Signs the request by adding authorization headers or query parameters depending
 	// on the token-type used.
-	const newRequestOptions = token.sign(requestOptions as ClientOAuth2RequestObject);
+	const newRequestOptions = token.sign(requestOptions);
 	const newRequestHeaders = (newRequestOptions.headers = newRequestOptions.headers ?? {});
 	// If keep bearer is false remove the it from the authorization header
 	if (oAuth2Options?.keepBearer === false && typeof newRequestHeaders.Authorization === 'string') {
@@ -1197,7 +1188,7 @@ export async function requestOAuth2(
 					credentialsType,
 					credentials,
 				);
-				const refreshedRequestOption = newToken.sign(requestOptions as ClientOAuth2RequestObject);
+				const refreshedRequestOption = newToken.sign(requestOptions);
 
 				if (oAuth2Options?.keyToIncludeInAccessTokenHeader) {
 					Object.assign(newRequestHeaders, {
@@ -1282,7 +1273,7 @@ export async function requestOAuth2(
 				);
 
 				// Make the request again with the new token
-				const newRequestOptions = newToken.sign(requestOptions as ClientOAuth2RequestObject);
+				const newRequestOptions = newToken.sign(requestOptions);
 				newRequestOptions.headers = newRequestOptions.headers ?? {};
 
 				if (oAuth2Options?.keyToIncludeInAccessTokenHeader) {
@@ -1385,7 +1376,7 @@ export async function httpRequestWithAuthentication(
 			return await requestOAuth2.call(
 				this,
 				credentialsType,
-				requestOptions,
+				requestOptions as clientOAuth2.RequestObject,
 				node,
 				additionalData,
 				additionalCredentialOptions?.oauth2,
@@ -1581,7 +1572,7 @@ export async function requestWithAuthentication(
 			return await requestOAuth2.call(
 				this,
 				credentialsType,
-				requestOptions,
+				requestOptions as clientOAuth2.RequestObject,
 				node,
 				additionalData,
 				additionalCredentialOptions?.oauth2,
@@ -2200,8 +2191,7 @@ const getRequestHelperFunctions = (
 		);
 	},
 
-	request: async (uriOrObject, options) =>
-		proxyRequestToAxios(workflow, additionalData, node, uriOrObject, options),
+	request: async (uriOrObject) => proxyRequestToAxios(workflow, additionalData, node, uriOrObject),
 
 	async requestWithAuthentication(
 		this,
@@ -2231,7 +2221,7 @@ const getRequestHelperFunctions = (
 	async requestOAuth2(
 		this: IAllExecuteFunctions,
 		credentialsType: string,
-		requestOptions: OptionsWithUri | requestPromise.RequestPromiseOptions,
+		requestOptions: clientOAuth2.RequestObject,
 		oAuth2Options?: IOAuth2Options,
 	): Promise<any> {
 		return requestOAuth2.call(
